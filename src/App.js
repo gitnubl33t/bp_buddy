aa62c03a5a0ad1f0f132afa6fb66492bc1595142
gitnubl33t <jessicad8695@gmail.com>
2021-01-02 19:47:45 -0800

    added nav html

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
+      <nav>
+        <a href="/">About</a>
+        <a href="/">Contact</a>
+        <a href="/">Home</a>
+      </nav>
     </div>
   );
 }
