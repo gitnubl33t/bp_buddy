71953463a6ad01017ff4522bf62c38ffa8e7d3f6
gitnubl33t <jessicad8695@gmail.com>
2021-01-02 18:03:14 -0800

    added svg logo to background, changed some css.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
-      <img src={buddyUrl} alt="buddy" />
+      <BuddyImg />
     </div>
   );
 }
