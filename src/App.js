69de8d91eae5ecea8f0287936c2faaa3933f5f81
gitnubl33t <jessicad8695@gmail.com>
2021-01-02 18:41:12 -0800

    background SVG is properly positioned and scaled

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import "./App.css";
-import buddyUrl, { ReactComponent as BuddyImg } from "./bp_buddy_bg_img.svg";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
-      <BuddyImg />
     </div>
   );
 }
